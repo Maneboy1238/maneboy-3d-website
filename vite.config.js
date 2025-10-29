7516e9d74705a22104a8b29aabedd9461500aa6b
maneboy <okonudumanasseh@gmail.com>
2025-10-29 07:53:10 +0000

    fix a broken path

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/maneboy-3d-website/', // Replace with your GitHub repository name
+  base: '/', // Replace with your GitHub repository name
 })
